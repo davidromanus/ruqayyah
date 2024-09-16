433065eb27a1566e0f264d41770dd672b981d6ba
panther <davidromanus19@gmail.com>
2024-09-16 14:40:08 +0100

    Fix styles again oh. Chineke merem ebere

--- app/ui/quickActionCard.ui.js
+++ app/ui/quickActionCard.ui.js
@@ -1,7 +1,7 @@
 export default function QuickAction({icon, title, info}){
     return (
       <div className="w-[32.8%] p-2 md:border rounded-md flex gap-2 flex-shrink-0">
-        <div className="w-full md:bg-purple-800 hover:bg-purple-900 transition duration-300 hover:cursor-pointer flex flex-col justify-center items-center py-3 lg:p-2 rounded-md lg:w-12 h-12 text-purple-800 md:text-white">
+        <div className="w-full md:bg-purple-800 hover:bg-purple-900 transition duration-300 hover:cursor-pointer flex flex-col justify-center items-center py-3 lg:p-2 rounded-md lg:w-12 h-12 text-purple-800 hover:text-white md:text-white">
           <span>{icon}</span>
           <p className="block lg:hidden text-[9px] font-medium">{title}</p>
         </div>
